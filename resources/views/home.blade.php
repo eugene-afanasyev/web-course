4922c0416559fa05d3fb7e7474fe674e7d2ab7f1
eugene-afanasyev <whoaeugene@gmail.com>
2021-01-31 00:55:43 +0900

    defined project layout

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="jumbotron">
-    <h1>Home page</h1>
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'MainController@index');
+Route::get('/categories', 'MainController@categories');
+Route::get('/mobiles/iphone', 'MainController@product');
 
-//Route::get('users/', [UserController::class, 'show']);
-
-//Route::get('products/', [ProductController::class, 'rawShow']);
-
-Route::get('categories/', function () {
-    return view('categories');
-});
